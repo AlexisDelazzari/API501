e6e91c2b52697465b3887bc485d47c7b3a9d4228
AlexisDelazzari <adelazzari8@gmail.com>
2023-11-11 02:47:10 +0100

    .env local now

--- src/config/ormconfig.ts
+++ src/config/ormconfig.ts
@@ -4,11 +4,11 @@ import 'dotenv/config'
 
 export const dataSource = new DataSource({
   namingStrategy: new NamingStrategy(),
-  type: "mariadb",
+  type: process.env.DB_TYPE === 'mysql' ? 'mysql' : 'mariadb',
   host: process.env.DB_HOST,
   port: 3306,
   username: process.env.DB_USERNAME ,
-  password: process.env.DB_PASSWORD ,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   synchronize: false,
   logging: true,
